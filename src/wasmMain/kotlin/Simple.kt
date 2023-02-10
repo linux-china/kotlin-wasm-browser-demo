3e791f4d9e7078fb5fd03fc6ddf875519dbf0abf
Libing Chen <libing.chen@gmail.com>
2023-02-10 11:52:33 +0800

    chore: polishing

--- src/wasmMain/kotlin/Simple.kt
+++ src/wasmMain/kotlin/Simple.kt
@@ -1,5 +1,7 @@
+import kotlinx.browser.document
+
 fun main() {
     val p = document.createElement("p")
     p.innerHTML = "Hello World!"
-    document.body.appendChild(p)
+    document.body!!.appendChild(p)
 }
