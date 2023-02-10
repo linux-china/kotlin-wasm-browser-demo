40b0f3ceb4d35d51235f8ec1049140e358b3dab6
Libing Chen <libing.chen@gmail.com>
2023-02-10 11:24:44 +0800

    build: update to kotlin 1.8.20-Beta

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,34 +1,19 @@
-import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
+@file:Suppress("OPT_IN_USAGE")
 
 plugins {
-    kotlin("multiplatform") version "1.8.0"
+    kotlin("multiplatform") version "1.8.20-Beta"
 }
 
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenLocal()
     mavenCentral()
-    maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
 }
 
 
 kotlin {
     wasm {
         binaries.executable()
-        browser {
-            commonWebpackConfig {
-                devServer = KotlinWebpackConfig.DevServer(
-                    open = mapOf(
-                        "app" to mapOf(
-                            "name" to "google chrome",
-                            "arguments" to listOf("--js-flags=--experimental-wasm-gc")
-                        )
-                    ),
-                    static = devServer?.static,
-                    port = 8000
-                )
-            }
-        }
+        browser()
     }
 }
