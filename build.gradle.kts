ec47be2400aa1162fc6cb2a71e18c8de7ce7ea98
Libing Chen <libing.chen@gmail.com>
2023-04-18 14:39:43 +0800

    build: polishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
 @file:Suppress("OPT_IN_USAGE")
 
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
+import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
+
 plugins {
     kotlin("multiplatform") version "1.8.20"
 }
@@ -14,6 +17,29 @@ repositories {
 kotlin {
     wasm {
         binaries.executable()
-        browser()
+        browser {
+            commonWebpackConfig {
+                devServer = (devServer ?: KotlinWebpackConfig.DevServer()).copy(
+                    open = mapOf(
+                        "app" to mapOf(
+                            "name" to "google chrome",
+                        )
+                    ),
+                )
+            }
+        }
+    }
+    sourceSets {
+        val commonMain by getting
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+        val wasmMain by getting
+        val wasmTest by getting
     }
 }
+
+// Use a proper version of webpack, TODO remove after updating to Kotlin 1.9.
+rootProject.the<NodeJsRootExtension>().versions.webpack.version = "5.76.2"
